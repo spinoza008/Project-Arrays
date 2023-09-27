2fadbb8c4dbd9fce942af23d15f885803293c439
spinoza008 <spinozadelva347@hotmail.com>
2023-09-26 21:56:57 -0400

    Add project files.

---
+++ Project Arrays/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Project_Arrays
+{
+    partial class frmArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArrays = new System.Windows.Forms.Button();
+            this.btnArrayInitialized = new System.Windows.Forms.Button();
+            this.btnAccessingUpdating = new System.Windows.Forms.Button();
+            this.btnSearchArray = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.btnArrayMath = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnInconveniant = new System.Windows.Forms.Button();
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.cboZodiac = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnArrays
+            // 
+            this.btnArrays.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArrays.Location = new System.Drawing.Point(2, 12);
+            this.btnArrays.Name = "btnArrays";
+            this.btnArrays.Size = new System.Drawing.Size(103, 34);
+            this.btnArrays.TabIndex = 0;
+            this.btnArrays.Text = "Arrays ";
+            this.btnArrays.UseVisualStyleBackColor = true;
+            this.btnArrays.Click += new System.EventHandler(this.btnArrays_Click);
+            // 
+            // btnArrayInitialized
+            // 
+            this.btnArrayInitialized.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArrayInitialized.Location = new System.Drawing.Point(111, 12);
+            this.btnArrayInitialized.Name = "btnArrayInitialized";
+            this.btnArrayInitialized.Size = new System.Drawing.Size(194, 45);
+            this.btnArrayInitialized.TabIndex = 1;
+            this.btnArrayInitialized.Text = "Array initialized in declaration";
+            this.btnArrayInitialized.UseVisualStyleBackColor = true;
+            this.btnArrayInitialized.Click += new System.EventHandler(this.btnArrayInitialized_Click);
+            // 
+            // btnAccessingUpdating
+            // 
+            this.btnAccessingUpdating.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccessingUpdating.Location = new System.Drawing.Point(2, 106);
+            this.btnAccessingUpdating.Name = "btnAccessingUpdating";
+            this.btnAccessingUpdating.Size = new System.Drawing.Size(202, 33);
+            this.btnAccessingUpdating.TabIndex = 2;
+            this.btnAccessingUpdating.Text = "Accessing and Updating ";
+            this.btnAccessingUpdating.UseVisualStyleBackColor = true;
+            this.btnAccessingUpdating.Click += new System.EventHandler(this.btnAccessingUpdating_Click);
+            // 
+            // btnSearchArray
+            // 
+            this.btnSearchArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchArray.Location = new System.Drawing.Point(232, 106);
+            this.btnSearchArray.Name = "btnSearchArray";
+            this.btnSearchArray.Size = new System.Drawing.Size(145, 33);
+            this.btnSearchArray.TabIndex = 3;
+            this.btnSearchArray.Text = "Search an Array ";
+            this.btnSearchArray.UseVisualStyleBackColor = true;
+            this.btnSearchArray.Click += new System.EventHandler(this.btnSearchArray_Click);
+            // 
+            // btnResize
+            // 
+            this.btnResize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResize.Location = new System.Drawing.Point(2, 156);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(131, 32);
+            this.btnResize.TabIndex = 4;
+            this.btnResize.Text = "Resizing Array ";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // btnArrayMath
+            // 
+            this.btnArrayMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArrayMath.Location = new System.Drawing.Point(232, 156);
+            this.btnArrayMath.Name = "btnArrayMath";
+            this.btnArrayMath.Size = new System.Drawing.Size(145, 32);
+            this.btnArrayMath.TabIndex = 5;
+            this.btnArrayMath.Text = "Array Math ";
+            this.btnArrayMath.UseVisualStyleBackColor = true;
+            this.btnArrayMath.Click += new System.EventHandler(this.btnArrayMath_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(395, 106);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(171, 33);
+            this.btnSort.TabIndex = 7;
+            this.btnSort.Text = "Sorting Array";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnInconveniant
+            // 
+            this.btnInconveniant.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInconveniant.Location = new System.Drawing.Point(395, 156);
+            this.btnInconveniant.Name = "btnInconveniant";
+            this.btnInconveniant.Size = new System.Drawing.Size(171, 47);
+            this.btnInconveniant.TabIndex = 6;
+            this.btnInconveniant.Text = "Array could be inconvenient";
+            this.btnInconveniant.UseVisualStyleBackColor = true;
+            this.btnInconveniant.Click += new System.EventHandler(this.btnInconveniant_Click);
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(2, 209);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(564, 96);
+            this.rtbDisplay.TabIndex = 8;
+            this.rtbDisplay.Text = "";
+            // 
+            // cboZodiac
+            // 
+            this.cboZodiac.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboZodiac.FormattingEnabled = true;
+            this.cboZodiac.Location = new System.Drawing.Point(362, 36);
+            this.cboZodiac.Name = "cboZodiac";
+            this.cboZodiac.Size = new System.Drawing.Size(204, 26);
+            this.cboZodiac.TabIndex = 9;
+            this.cboZodiac.SelectedIndexChanged += new System.EventHandler(this.cboZodiac_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(359, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Zodiac signs:";
+            // 
+            // frmArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboZodiac);
+            this.Controls.Add(this.rtbDisplay);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnInconveniant);
+            this.Controls.Add(this.btnArrayMath);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.btnSearchArray);
+            this.Controls.Add(this.btnAccessingUpdating);
+            this.Controls.Add(this.btnArrayInitialized);
+            this.Controls.Add(this.btnArrays);
+            this.Name = "frmArray";
+            this.Text = "Project Array";
+            this.Load += new System.EventHandler(this.frmArray_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArrays;
+        private System.Windows.Forms.Button btnArrayInitialized;
+        private System.Windows.Forms.Button btnAccessingUpdating;
+        private System.Windows.Forms.Button btnSearchArray;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button btnArrayMath;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnInconveniant;
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+        private System.Windows.Forms.ComboBox cboZodiac;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Project Arrays/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Arrays
+{
+    public partial class frmArray : Form
+    {
+        public frmArray()
+        {
+            InitializeComponent();
+        }
+        string[] zodiacSign = new string[12];
+        string[] zodiacDate = new string[12];
+
+        private void btnArrays_Click(object sender, EventArgs e)
+        {
+            string[] teamMembers = new string[5];
+            teamMembers[0] = "Jane";
+            teamMembers[1] = "Jack";
+            teamMembers[2] = "Timothy";
+            teamMembers[3] = "Mary";
+            teamMembers[4] = "Elizabeth";
+            for (int x=0; x<5; x++)
+            {
+                rtbDisplay.AppendText(teamMembers[x] + Environment.NewLine);
+            }
+        }
+        double[] monthlySales = new double[3];
+
+        private void btnSearchArray_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Clear();
+            rtbDisplay.Text = "Monthly Sales under 2000 are listed below:" + Environment.NewLine;
+            double y;
+            for(int x = 0; x < monthlySales.Length; x++)
+            {
+                y = monthlySales[x];
+                if (y<2000)
+                {
+                    rtbDisplay.AppendText(y.ToString() + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnArrayMath_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Clear();
+            foreach(double x in monthlySales)
+            {
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            }
+            double total= 0, average, largest, smallest;
+            foreach(double x in monthlySales)
+            {
+                total += x;
+            }
+            average = total / monthlySales.Length;
+            largest = monthlySales[0];
+            for(int x = 1; x <monthlySales.Length;x++)
+            {
+                if (monthlySales[x]>largest)
+                {
+                    largest = monthlySales[x];
+                }
+            }
+            smallest = monthlySales[0];
+            for(int x = 1; x <monthlySales.Length; x++)
+            {
+                if (monthlySales[x] < smallest)
+                {
+                    smallest = monthlySales[x];
+                }
+            }
+            rtbDisplay.AppendText("Total Sales:" + total.ToString() + Environment.NewLine + "Average Sales:" + average.ToString() + Environment.NewLine + "largest Sales:" + largest.ToString() + Environment.NewLine +
+                "smallest sales:" + smallest.ToString());
+
+        }
+
+        private void btnArrayInitialized_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Clear();
+            string[] phones = new string[] { "Iphone", "Samsung", "Nokia Lumia" };
+            for(int x = 0; x <3; x++)
+            {
+                rtbDisplay.AppendText(phones[x] + Environment.NewLine);
+            }
+            foreach(string x in phones)
+            {
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            }
+            int y = 0;
+            while(y < phones.Length)
+            {
+                rtbDisplay.AppendText(y + Environment
+                    .NewLine);
+                y++;
+            }
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref monthlySales, 6);
+            monthlySales[3] = 3913;
+            monthlySales[4] = 2878;
+            monthlySales[5] = 3110;
+            foreach(double x in monthlySales)
+            {
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Text = "Before sorting:" + Environment.NewLine;
+
+            foreach (double x in monthlySales)
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            foreach (double x in monthlySales)
+                rtbDisplay.AppendText(x + Environment.NewLine);
+        }
+
+        private void btnAccessingUpdating_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Text = "The 3rd Zodiac sign is :" + zodiacSign[2];
+            zodiacSign[6] = "Libra the Scale";
+            foreach(string x in zodiacSign)
+            {
+                rtbDisplay.AppendText(Environment.NewLine + x);
+            }
+        }
+
+        private void btnInconveniant_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref zodiacSign, 13);
+            for (int x = 11; x>=5; x--)
+            {
+                zodiacSign[x + 1] = zodiacSign[x];
+            }
+            zodiacSign[5] = "Loxodonta The Elephant";
+            rtbDisplay.Clear();
+            foreach(string x in zodiacSign)
+            {
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            }
+            for (int x = 1; x <=11; x++)
+            {
+                zodiacSign[x] = zodiacSign[x + 1];
+            }
+            Array.Resize(ref zodiacSign, 12);
+            rtbDisplay.AppendText(@"After adding elephant and remove bull the following zodiac signs remains:" + Environment.NewLine);
+            foreach(string x in zodiacSign)
+            {
+                rtbDisplay.AppendText(x + Environment.NewLine);
+            }
+        }
+
+        private void cboZodiac_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int x = cboZodiac.SelectedIndex;
+            rtbDisplay.Text = zodiacDate[x];
+        }
+        string[] monthName = new string[3];
+
+        private void frmArray_Load(object sender, EventArgs e)
+        {
+            monthlySales[0] = 2300;
+            monthlySales[1] = 1938;
+            monthlySales[2] = 2399;
+            monthName[0] = "January";
+            monthName[1] = "February";
+            monthName[2] = "March";
+
+            zodiacSign[0] = "Aries The Ram";
+            zodiacDate[0] = "Mar. 21 Apr 19";
+            zodiacSign[1] = "Taurus The Bull";
+            zodiacDate[1] = "Apr 20 May 20";
+            zodiacSign[2] = "Gemini The Twins";
+            zodiacDate[2] = "May 21 June 21";
+            zodiacSign[3] = "Cancer The Carb";
+            zodiacDate[3] = "June July 22";
+            zodiacSign[4] = "Leo the Lion";
+            zodiacDate[4] = "July 23 Aug. 22";
+            zodiacSign[5] = "Virgo The Virgin";
+            zodiacDate[5] = "Aug 23 Step. 22";
+            zodiacSign[6] = "Libra the Balance";
+            zodiacDate[6] = "Sept . 23 Oct. 23";
+            zodiacSign[7] = "Scorpio The Scorpion";
+            zodiacDate[7] = "Oct. 24 Nov. 21";
+            zodiacSign[8] = "Sagittarius the Archer";
+            zodiacDate[8] = "Nov. 22 Dec. 21";
+            zodiacSign[9] = "Capricorn The Goat";
+            zodiacDate[9] = "Dec. 22 Jan. 19";
+            zodiacSign[10] = "Aquarius The Water Bearer";
+            zodiacDate[10] = "Jan. 20 Feb. 18";
+            zodiacSign[11] = "Pisces the Firshes";
+            zodiacDate[11] = "Feb. 19 Mar. 20";
+            foreach(string x in zodiacSign)
+            {
+                cboZodiac.Items.Add(x);
+
+            }
+            cboZodiac.SelectedIndex = 0;
+
+
+        }
+    }
+}
